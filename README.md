e15619c6aecc6292f305a441d8eb148a7a8c9027
Simon Huwiler <webmaster@simonhuwiler.ch>
2022-05-18 10:03:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Steuerrechner Scraper
+## Installation
+Clone Repo:
+```
+git clone git@github.com:simonhuwiler/nzz_steuerrechner_scraper.git
+```
+
+Create virtual env (python3!): Open folder in terminal
+```
+python -m venv
+# or
+python3 -m venv
+```
+
+Activate Virtual Env
+```
+source env/bin/activate
+```
+
+Install requirements
+```
+pip install -r requirements.txt
+```
+## Usage
+* `src/0_export.ipynb` Scrapes the file
+* `src/1_extract.ipynb` Extracts the data
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+massExportModel = {
+    1: "Ledig, ohne Kinder",
+    2: "Ledig, mit 1 Kind",
+    3: "Ledig, mit 2 Kindern",
+    4: "Ledig, mit 3 Kindern",
+    5: "Verheiratet, ein Einkommen, ohne Kinder",
+    6: "Verheiratet, ein Einkommen, mit 1 Kind",
+    7: "Verheiratet, ein Einkommen, mit 2 Kindern",
+    8: "Verheiratet, ein Einkommen, mit 3 Kindern",
+    9: "Verheiratet, zwei Einkommen (70/30), ohne Kinder",
+    10: "Verheiratet, zwei Einkommen (70/30), mit 1 Kind",
+    11: "Verheiratet, zwei Einkommen (70/30), mit 2 Kindern",
+    12: "Verheiratet, zwei Einkommen (70/30), mit 3 Kindern",
+    13: "Verheiratet, zwei Einkommen (50/50), ohne Kinder",
+    14: "Verheiratet, zwei Einkommen (50/50), mit 1 Kind",
+    15: "Verheiratet, zwei Einkommen (50/50), mit 2 Kindern",
+    16: "Verheiratet, zwei Einkommen (50/50), mit 3 Kindern",
+    17: "Rentner/in, ledig",
+    18: "Verheiratet, eine Rente",
+    19: "Verheiratet, zwei Renten (70/30)",
+    20: "Verheiratet, zwei Renten (50/50)"
+}
+
+# What happend to id 2 and 4?
+confessions = {
+    0: 'reformiert',
+    1: 'römisch-katholisch',
+    3: 'christkatholisch',
+    5: 'Andere'
+}
